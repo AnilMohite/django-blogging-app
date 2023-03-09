69326cd49d55d1bd33b92f00d055d15e0de54c9b
Anil Mohite <30444194+AnilMohite@users.noreply.github.com>
2023-03-09 20:20:34 +0530

    Update models.py

--- blogs/models.py
+++ blogs/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Blog(models.Model):
     user_id = models.ForeignKey(User,on_delete=models.PROTECT,default=1)
     title = models.CharField(max_length=100)
